2437f161117ecc158def1759e478c3d21dc9cd69
Aliou Diallo <code@aliou.me>
2014-02-27 20:00:05 +0100

    Add tests for the starred entries match.

--- test/DayOne.js
+++ test/DayOne.js
@@ -17,7 +17,7 @@ describe('DayOne', function() {
             var day1 = new DayOne(mockConfig);
 
             day1.list(function(error, entries) {
-                assert.equal(entries.length, 2, 'Two entries');
+                assert.equal(entries.length, 3, 'Three entries');
 
                 done();
             });
@@ -35,8 +35,36 @@ describe('DayOne', function() {
 
                 done();
             });
-        })
+        });
 
+        it('should list all entries matching the starred option', function(done) {
+            var day1 = new DayOne(mockConfig);
+
+            day1.list({
+              starred: false
+            }, function(error, entries) {
+                assert.equal(entries.length, 2, 'Two entries');
+                assert.equal(entries[0].starred, false, 'Matches the starred option');
+
+                done();
+            });
+        });
+
+        it('should list all entries matching the starred option and the tags', function(done) {
+            var day1 = new DayOne(mockConfig);
+
+            day1.list({
+              starred: true,
+              tags: ['othertag']
+            }, function(error, entries) {
+                assert.equal(entries.length, 1, 'One entry');
+                assert.equal(entries[0].tags.indexOf('othertag'), 0, 'Needs to include tag othertag');
+                assert.equal(entries[0].starred, true, 'Matches the starred option');
+                assert.equal(entries[0].text, true, 'Tagged and starred entry');
+
+                done();
+            });
+        });
     });
 
     describe('#save', function() {
